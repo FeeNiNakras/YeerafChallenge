a5a1f7fe936bf79c21daeb4c4f2f45d7eb4bc901
FeeNiNakras <49543312+FeeNiNakras@users.noreply.github.com>
2022-05-23 00:05:28 +0700

    Update challenge_1.js

--- challenge_1.js
+++ challenge_1.js
@@ -1,15 +1,22 @@
 console.log("Start challenge 1!");
 
-let result = "";
-for(let i = 1; i < 16; i++){
-    if((i % 3 == 0) && (i % 5  == 0)){
-        result = "ThreeFive";
-    }else if(i % 3 == 0){
-        result = "Three";
-    }else if(i % 5  == 0){
-        result = "Five";
-    }else{
+const obj = {
+    3: "Three",
+    5: "Five",
+    11: "Eleven"
+}
+
+for(let i = 1; i < 101; i++){
+    let result = "";
+    for (const [key, value] of Object.entries(obj)) {
+        if(i % key == 0){
+            result = result+value;
+        }
+    }
+
+    if(result == ""){
         result = i;
     }
+
     console.log(result);
 }
